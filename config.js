e6b07d82138b73d53268c874ee3f519cf3bf8071
SantiGrimoldi <Santiago.grimoldi@ing.austral.edu.ar>
2023-10-25 10:46:11 -0300

    initial commit

--- config.js
+++ config.js
@@ -4,7 +4,7 @@ config.debug = process.env.DEBUG || true;
 
 config.mqtt  = {};
 config.mqtt.namespace = process.env.MQTT_NAMESPACE || '#';
-config.mqtt.hostname  = process.env.MQTT_HOSTNAME  || '18.232.60.65';
+config.mqtt.hostname  = process.env.MQTT_HOSTNAME  || '54.211.69.39';
 config.mqtt.port      = process.env.MQTT_PORT      || 1883;
 
 config.mongodb = {};
