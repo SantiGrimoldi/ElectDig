dfabf3711e8834c8a4f3e8cd6646e8254ab93abf
ppanosyan <pedro.panosyan@ing.autral.edu.ar>
2023-11-08 11:38:54 -0300

    abstracted write_database

--- DBWriter.js
+++ DBWriter.js
@@ -5,13 +5,15 @@ const mongoUri = 'mongodb://127.0.0.1:27017';
 const client = new MongoClient(mongoUri);
 const database = client.db('elect_dig');
 const messageCollection = database.collection('vending_machine');
+const historyCollection = database.collection('history');
 
-async function write_database(product, qty){
+async function write_database(product, old_qty, update_qty){
     try {
         await messageCollection.updateOne(
             {title: product},
-            {$inc: {qty: parseInt(qty.toString())}}
+            {$inc: {qty: parseInt(update_qty.toString())}}
         )
+        await add_history(product, old_qty, update_qty)
         return "Transaccion exitosa";
     } catch (e) {
         return e.message
@@ -25,7 +27,7 @@ async function buy_product(topic, message) {
             {title: product}
         )
         if (before.qty < 0) return "No hay stock";
-        return write_database(before.title, message)
+        return write_database(before.title, before.qty, message)
     } catch (e) {
         return e.message
     }
@@ -37,7 +39,7 @@ async function repose_product(topic, message) {
         const before = await messageCollection.findOne(
             {title: product}
         )
-        return write_database(before.title, message)
+        return write_database(before.title, before.qty, message)
     } catch (e) {
         return e.message
     }
@@ -67,6 +69,24 @@ async function read_product(topic) {
     } catch (e) {
         return e
     }
+}
+
+async function add_history(product, old_qty, update_number) {
+    const date_time = new Date();
+    try {
+        await historyCollection.insertOne(
+            {
+                title: product,
+                old_qty: old_qty,
+                update_qty: update_number,
+                date: date_time
+            }
+        )
+        return "Historial agregado"
+    } catch (e) {
+        return e
+    }
+
 }
 
 async function read_all_products() {
