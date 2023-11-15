c60bc2b333b1826ec29af35b5f036e11f9684b5d
ppanosyan <pedro.panosyan@ing.autral.edu.ar>
2023-11-15 11:10:09 -0300

    abstracted write_database

--- DBWriter.js
+++ DBWriter.js
@@ -91,7 +91,7 @@ async function add_history(product, old_qty, update_number) {
 async function read_history(topic) {
     const product = splitPath(topic, -1)
     try {
-        return await historyCollection.find({title: product});
+        return await historyCollection.find({title: product}).toArray();
     } catch (e) {
         return e
     }
